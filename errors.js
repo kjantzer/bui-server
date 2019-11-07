3a89ce3b7b248856a871bc32fde9fd4ad44cbb29
Kevin Jantzer <kevin.jantzer@blackstoneaudio.com>
2019-11-07 14:02:16 -0800

    Add EmailError

--- errors.js
+++ errors.js
@@ -6,6 +6,14 @@ module.exports = {
             this.name = 'AccessError'
             this.code = code
         }
+    },
+
+    EmailError: class extends Error {
+        constructor(msg='email error', code=400){
+            super(msg)
+            this.name = 'EmailError'
+            this.code = code
+        }
     }
 
 }
